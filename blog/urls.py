d9656625c3302afe8c4b019be31fb9a84053322c
Shiv_Boy <shivamchouhan728@gmail.com>
2020-09-26 13:18:56 +0530

    adding-files

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('profile/<str:username>/', views.get_user_profile, name='user-profile'),
+    path('search/', views.search, name='search_results'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
+]
